8a6bd053b70c698bce0a6677ee8d47623db20ef3
atom36942123 <atom36942@gmail.com>
2024-10-07 18:18:42 +0530

    commit

---
+++ project/graphqll.py
@@ -0,0 +1,37 @@
+#run from script folder=fastapi dev graphqll.py
+#open=http://localhost:8000/graphql
+#paste below code
+"""
+{
+  user {
+    name
+    age
+  }
+}
+"""
+
+import strawberry
+from fastapi import FastAPI
+from strawberry.asgi import GraphQL
+
+
+@strawberry.type
+class User:
+    name: str
+    age: int
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def user(self) -> User:
+        return User(name="Patrick", age=100)
+
+
+schema = strawberry.Schema(query=Query)
+
+
+graphql_app = GraphQL(schema)
+
+app = FastAPI()
+app.add_route("/graphql", graphql_app)
+app.add_websocket_route("/graphql", graphql_app)
